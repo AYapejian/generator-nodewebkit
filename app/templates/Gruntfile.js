b9c4f1b21bdf4f283c05c412bc1ce5424b63de41
ay255001 <ara.yapejian@teradata.com>
2013-09-23 14:24:31 -0400

    mend

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -2,10 +2,29 @@
 'use strict';
  
 module.exports = function (grunt) {
-  // load all grunt tasks
-  require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
  
   grunt.initConfig({
-    
+    pkg: grunt.file.readJSON('package.json')
+  });
+
+  // TODO: Build the node-webkit apps for Win, OSX and Linux
+  // https://github.com/rogerwang/node-webkit/wiki/How-to-package-and-distribute-your-apps
+  grunt.registerTask('build', ['build_win', 'build_osx', 'build_linux']);
+
+  grunt.registerTask('build_win', '', function () {
+    grunt.log.write('Building Windows: ').ok();
+
+    // copy /b nw.exe+app.nw app.exe 
+  
+  });
+
+  grunt.registerTask('build_osx', '', function () {
+    grunt.log.write('Building OSX: ').ok();
+  
+  });
+
+  grunt.registerTask('build_linux', '', function () {
+    grunt.log.write('Building Linux: ').ok();
+  
   });
 };
